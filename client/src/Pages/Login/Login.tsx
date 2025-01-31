22b9ab48614755f6054e5fac4c7bb5ce25754fc2
Sadre321 <haydaremre31@gmail.com>
2025-02-01 01:29:09 +0300

    Login ekrani olusturuldu

--- client/src/Pages/Login/Login.tsx
+++ client/src/Pages/Login/Login.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="my-5">
+      <h1 className="text-5xl font-bold mb-5">Login</h1>
+      <p className="opacity-75 text-lg mb-10">
+        Geri eklemeler mi yapicaksin. Hosgeldin!
+      </p>
+      <hr className="opacity-15" />
 
-export default Login
+      <form action="" className="py-10 mb-16">
+        <div className="mb-4">
+          <label htmlFor="email" className="block font-medium mb-3 px-2">
+            E-posta
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="px-4 py-3 rounded-3xl w-full"
+            placeholder="E-posta adresinizi girin"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password" className="block font-medium mb-3 px-2">
+            Şifre
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="px-4 py-3 rounded-3xl w-full"
+            placeholder="Şifrenizi girin"
+            required
+          />
+        </div>
+        <div className="">
+          <button
+            type="submit"
+            className="px-6 py-2 bg-orange-400 rounded-3xl text-lg font-semibold mt-3"
+          >
+            Giriş Yap
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
